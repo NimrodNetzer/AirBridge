d2596be3b435884185cbb1e8eacdf52959c626ef
NimrodNetzer <nimrodnetzer@gmail.com>
2026-03-27 17:09:56 +0300

    fix(android): add missing kotlinx.coroutines.flow.first import in DeviceConnectionService

--- android/app/src/main/java/com/airbridge/app/core/DeviceConnectionService.kt
+++ android/app/src/main/java/com/airbridge/app/core/DeviceConnectionService.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
